3a9c3d80c74971cd7e4f43cfea6582a7faabaaf0
junqfisica <junqfisica@hotmail.com>
2020-10-26 17:09:07 +0100

    Thiago - fixed setup license.

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 import setuptools
 
+INSTALL_REQUIRES = ["numpy", "scipy >= 1.4.0", "obspy"]
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="cwt-seismology",  # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="rcabdia",
     author_email="rcabdia@roa.es",
     description="Wavelet transform for seismic signal detection.",
@@ -15,8 +17,10 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU Lesser General Public License, Version 3 (LGPLv3)",
+        'License :: OSI Approved :: GNU Library or ' +
+                'Lesser General Public License (LGPL)',
         "Operating System :: OS Independent",
     ],
+    install_requires=INSTALL_REQUIRES,
     python_requires='>=3.6',
 )
